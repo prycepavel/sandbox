486b4e96b498d4a2d83edd67e5d314ea1e387d3d
prycepavel <pryce.pavel@gmail.com>
2023-04-20 10:53:58 +0300

    nested loops and labels

--- Cycles/script.js
+++ Cycles/script.js
@@ -21,3 +21,38 @@ for (let i = 1; i < 10; i++){
   console.log(i);
 }
 
+// Вложенные циклы
+for (let i = 0; i < 3; i++){
+  console.log(i);
+  for (let j = 0; j < 3; j++){
+    console.log(j);
+  }
+}
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++){
+  
+  for (let j = 0; j < i; j++){
+    result += '*';
+  }
+
+  result += '\n';
+}
+
+console.log(result);
+
+
+first: for (let i = 0; i < 3; i++){
+  console.log(`First level: ${i}`);
+  
+  for (let j = 0; j < 3; j++){
+    console.log(`Second level: ${j}`);
+    
+    for (let k = 0; k < 5; k++){
+      if (k === 2) continue first; 
+      console.log(`Third level: ${k}`);
+    }
+  }
+}
