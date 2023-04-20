68955eb080b5957d5ce0c26982c4073af0cd9fad
prycepavel <pryce.pavel@gmail.com>
2023-04-20 09:29:15 +0300

    add cycles

---
+++ Cycles/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let num = 50;
+while (num <= 55) {
+  console.log(num);
+  num++;
+}
+
+let number = 50;
+do {
+  console.log(number);
+  number++;
+}
+while (number < 55);
+
+
+for (let i = 1; i < 10; i++){
+  if (i === 6) {
+    break;
+  }
+  console.log(i);
+}
+
